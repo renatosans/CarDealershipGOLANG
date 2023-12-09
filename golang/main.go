b119ce277400c62d69abd8a280bcb9f8a2083007
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-12-09 04:00:38 -0300

    Ajustes

---
+++ golang/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"cardealership/prisma/db"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	// "github.com/joho/godotenv"
+)
+
+func GetPrisma(c *gin.Context) *db.PrismaClient {
+	client := db.NewClient()
+	if err := client.Prisma.Connect(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return nil
+	}
+
+	return client
+}
+
+func main() {
+	// godotenv.Load(".env")
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Gin gonic API",
+		})
+	})
+
+	rGroup := router.Group("/api")
+	rGroup.GET("/pets", getPets)
+	rGroup.POST("/pets", postPet)
+	rGroup.PATCH("/pets/:id", patchPet)
+	rGroup.DELETE("/pets/:id", deletePet)
+
+	router.Use(cors.Default())
+	router.Run(":3000") // listen and serve on 0.0.0.0:3000
+}
