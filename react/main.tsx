630a469b46354005883490a4966dbbf5f8e76db7
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-12-09 16:35:54 -0300

    Ajustes

--- react/main.tsx
+++ react/main.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Layout from './components/Layout'
 
-// Frontend at http://0.0.0.0:5173/
+// Frontend at http://0.0.0.0:3000/
 // Backend at http://0.0.0.0:8080/api/cars
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
