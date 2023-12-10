e67e07508e08ee9d2e69429f2f5a042ec4a04d26
renatosans <renato@datacopy.com.br>
2023-12-10 09:27:27 -0300

    Ajustes

--- database/create_postgres.sql
+++ database/create_postgres.sql
@@ -14,6 +14,14 @@ CREATE TABLE public.cars_for_sale (
 	PRIMARY KEY (id)
 );
 
+CREATE TABLE public.cars_for_service (
+	id SERIAL,
+	customer_id int4 NOT NULL,
+	car_details varchar(160) NOT NULL,
+	mechanic varchar(160) NOT NULL,
+	PRIMARY KEY (id)
+);
+
 CREATE TABLE public.customer (
 	id SERIAL,
 	first_name varchar(50) NOT NULL,
