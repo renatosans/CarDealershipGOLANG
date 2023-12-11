92efc8c0da8c75fd9e1830b4dca666b1ae3229f9
renatosans <renato@datacopy.com.br>
2023-12-10 21:28:13 -0300

    Ajustes

--- golang/main.go
+++ golang/main.go
@@ -1,78 +1,12 @@
 package main
 
 import (
-	"cardealership/handlers"
-	"cardealership/prisma/db"
-	"cardealership/utils"
 	"net/http"
-	"strconv"
-
-	"github.com/gin-contrib/cors"
+	"cardealership/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	// "github.com/joho/godotenv"
 )
-
-func getCars(c *gin.Context) {
-	// var cars []db.InnerCarsForSale
-
-	client := utils.GetPrisma(c)
-
-	pets, err := client.CarsForSale.FindMany().Exec(c)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"data": pets})
-}
-
-func postCar(c *gin.Context) {
-	var payload db.InnerCarsForSale
-
-	// Bind JSON body to the struct
-	if err := c.ShouldBindJSON(&payload); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	client := utils.GetPrisma(c)
-	insertedCar, err := client.CarsForSale.CreateOne(
-		db.CarsForSale.Brand.Set(payload.Brand),
-		db.CarsForSale.Model.Set(payload.Model),
-		db.CarsForSale.Year.Set(payload.Year),
-		db.CarsForSale.Price.Set(payload.Price),
-		db.CarsForSale.Color.SetOptional(payload.Color),
-		db.CarsForSale.Mileage.SetOptional(payload.Mileage),
-	).Exec(c)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Car created successfully", "car": insertedCar})
-}
-
-func deleteCar(c *gin.Context) {
-	// TODO: utilizar o flag_removed ao invés de apagar o registro na tabela
-
-	idParam := c.Param("id")
-	id, err := strconv.Atoi(idParam)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
-		return
-	}
-
-	client := utils.GetPrisma(c)
-	deletedCar, err := client.CarsForSale.FindUnique(
-		db.CarsForSale.ID.Equals(id),
-	).Delete().Exec(c)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Car deleted successfully", "car id": deletedCar.ID})
-}
 
 func addCustomerHandlers(rGroup *gin.RouterGroup) {
 	rGroup.GET("/customers", handlers.GetCustomers)
